c1a653673ee5ba60347febae0789f7838d07de9d
Leonard Kelley <lkelley@svc.edu>
2016-08-24 17:04:30 -0400

    Updated the UI a bit to actually scale again instead of being fixed. button temporary until voice commands are up and running.

--- Zulrah Rotation Assistant/Main.Designer.cs
+++ Zulrah Rotation Assistant/Main.Designer.cs
@@ -26,40 +26,58 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.Canvas = new System.Windows.Forms.Panel();
             this.btnNextPhase = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Canvas
             // 
             this.Canvas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(48)))));
-            this.Canvas.Location = new System.Drawing.Point(10, 8);
-            this.Canvas.Margin = new System.Windows.Forms.Padding(10);
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(3, 3);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(337, 277);
+            this.Canvas.Size = new System.Drawing.Size(353, 292);
             this.Canvas.TabIndex = 0;
             this.Canvas.Resize += new System.EventHandler(this.Canvas_Resize);
             // 
             // btnNextPhase
             // 
-            this.btnNextPhase.Location = new System.Drawing.Point(272, 298);
+            this.btnNextPhase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNextPhase.Location = new System.Drawing.Point(3, 301);
             this.btnNextPhase.Name = "btnNextPhase";
-            this.btnNextPhase.Size = new System.Drawing.Size(75, 23);
+            this.btnNextPhase.Size = new System.Drawing.Size(353, 29);
             this.btnNextPhase.TabIndex = 1;
             this.btnNextPhase.Text = "Next Phase";
             this.btnNextPhase.UseVisualStyleBackColor = true;
             this.btnNextPhase.Click += new System.EventHandler(this.btnNextPhase_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Canvas, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnNextPhase, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(359, 333);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(359, 333);
-            this.Controls.Add(this.btnNextPhase);
-            this.Controls.Add(this.Canvas);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(50, 50);
             this.Name = "Main";
             this.Text = "Zulrah Rotation Assitant";
             this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -68,5 +86,6 @@
 
         private System.Windows.Forms.Panel Canvas;
         private System.Windows.Forms.Button btnNextPhase;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
